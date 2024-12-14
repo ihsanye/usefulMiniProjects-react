4c277ea67c9b7c8be0b9af84bc90b51b85fc2a8e
ihsanye <ihsany98@gmail.com>
2024-12-14 23:45:14 +0300

    open and close clicks

--- src/components/accordian/Accordian.jsx
+++ src/components/accordian/Accordian.jsx
@@ -5,17 +5,26 @@ function Accordian() {
 
     const [selected, setSelected] = useState(null)
 
+    const handleSingleSelection = (getCurrentId) => {
+        setSelected(getCurrentId === selected ? null : getCurrentId)
+    }
+
     return (
         <div className='wrapper'>
             <div className="accordian">
                 {
                     data && data.length > 0
-                        ? data.map(dataItem => <div className='item'>
-                            <div className='title'>
+                        ? data.map((dataItem) => (<div className='item'>
+                            <div onClick={() => handleSingleSelection(dataItem.id)} className='title'>
                                 <h3>{dataItem.question}</h3>
                                 <span>+</span>
                             </div>
-                        </div>)
+                            {
+                                selected === dataItem.id
+                                    ? <div className='content'>{dataItem.answer}</div>
+                                    : null
+                            }
+                        </div>))
                         : <div>No data</div>
                 }
             </div>
--- src/components/accordian/data.js
+++ src/components/accordian/data.js
@@ -1,20 +1,20 @@
 const data = [
     {
         id: '1',
-        question: 'Q1',
-        answer: 'A1'
+        question: '1-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio sunt eius ratione quaerat, dicta odit!',
+        answer: '1-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda recusandae dolorum consequatur nemo expedita corrupti laborum culpa, voluptates quis quo iure illo eos eum molestiae aspernatur error quia quibusdam tempore.'
     }, {
         id: '2',
-        question: 'Q2',
-        answer: 'A2'
+        question: '2-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio sunt eius ratione quaerat, dicta odit!',
+        answer: '2-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda recusandae dolorum consequatur nemo expedita corrupti laborum culpa, voluptates quis quo iure illo eos eum molestiae aspernatur error quia quibusdam tempore.'
     }, {
         id: '3',
-        question: 'Q3',
-        answer: 'A3'
+        question: '3-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio sunt eius ratione quaerat, dicta odit!',
+        answer: '3-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda recusandae dolorum consequatur nemo expedita corrupti laborum culpa, voluptates quis quo iure illo eos eum molestiae aspernatur error quia quibusdam tempore.'
     }, {
         id: '4',
-        question: 'Q4',
-        answer: 'A4'
+        question: '4-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio sunt eius ratione quaerat, dicta odit!',
+        answer: '4-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda recusandae dolorum consequatur nemo expedita corrupti laborum culpa, voluptates quis quo iure illo eos eum molestiae aspernatur error quia quibusdam tempore.'
     }
 ];
 export default data;
