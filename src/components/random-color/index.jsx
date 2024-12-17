dae7bfa4e86aa25f3a8787b28b06e8fac42218de
ihsanye <ihsany98@gmail.com>
2024-12-17 23:48:28 +0300

    first commit for random color generator

---
+++ src/components/random-color/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function RandomColor() {
+
+    const [typeOfColor, setTypeOfColor] = useState('hex');
+    const [color, setColor] = useState('#000000');
+
+    return (
+        <div className="container" style={{ width: '100vw', height: '100vh', background: color }} >
+            <button>Create HEX Color</button>
+            <button>Create RGB Color</button>
+            <button>Generate Random Color</button>
+        </div>
+    )
+}
