cc700870687ead3f4363df1d8b3a5bb5c781ee32
ihsanye <ihsany98@gmail.com>
2024-12-18 23:38:57 +0300

    rgb and hex color generator buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Accordian from './components/accordian/Accordian'
+import RandomColor from './components/random-color/RandomColor'
 
 function App() {
 
   return (
     <div>
       <Accordian />
+      <RandomColor />
     </div>
   )
 }
