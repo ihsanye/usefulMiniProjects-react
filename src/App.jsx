3e1df55cca34dbce7588c890d7a83a7ee01216d0
ihsanye <ihsany98@gmail.com>
2025-01-10 23:26:53 +0300

    image slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Accordian from './components/accordian/Accordian'
+import ImageSlider from './components/image-slider/ImageSlider'
 import RandomColor from './components/random-color/RandomColor'
 import StarRating from './components/star-rating/StarRating'
 
@@ -9,7 +10,11 @@ function App() {
     <div>
       <Accordian />
       <RandomColor />
-      <StarRating noOfStars={10}/>
+      <StarRating noOfStars={10} />
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"} />
     </div>
   )
 }
