f3768733e65c3f52403bd2d17d23ba4197c5a127
ihsanye <ihsany98@gmail.com>
2025-01-13 22:39:34 +0300

    load more data button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Accordian from './components/accordian/Accordian'
 import ImageSlider from './components/image-slider/ImageSlider'
+import LoadMoreData from './components/load-more/LoadMoreData'
 import RandomColor from './components/random-color/RandomColor'
 import StarRating from './components/star-rating/StarRating'
 
@@ -15,6 +16,7 @@ function App() {
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"} />
+      <LoadMoreData />
     </div>
   )
 }
