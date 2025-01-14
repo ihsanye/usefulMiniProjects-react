38d276862cf4e0aafeb92fbba02ec1c4edd5b50d
ihsanye <ihsany98@gmail.com>
2025-01-14 22:24:26 +0300

    qr code generator

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Accordian from './components/accordian/Accordian'
 import ImageSlider from './components/image-slider/ImageSlider'
 import LoadMoreData from './components/load-more/LoadMoreData'
+import QrCodeGenerator from './components/qr-coder/QrCodeGenerator'
 import RandomColor from './components/random-color/RandomColor'
 import StarRating from './components/star-rating/StarRating'
 
@@ -17,6 +18,7 @@ function App() {
         page={"1"}
         limit={"10"} />
       <LoadMoreData />
+      <QrCodeGenerator />
     </div>
   )
 }
